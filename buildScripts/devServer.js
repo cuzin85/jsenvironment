a3595caeb5cdd0ce6db83e0669efa03548fe5834
DESKTOP-E19AQIS\blabl <osappp@gmail.com>
2017-08-31 19:39:33 +0300

    add nsp

--- buildScripts/devServer.js
+++ buildScripts/devServer.js
@@ -6,13 +6,13 @@ const app = express();
 const port = 3010;
 
 app.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, '../src/index.html'));
+    res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, (err) => {
-    if (err){
+    if (err) {
         console.log(err);
     } else {
-        open('http://localhost:' + port);
+        open('http://localhost:' + port, "Chrome");
     }
 });
